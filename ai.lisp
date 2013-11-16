d5ea2fa0f133de32f4c1090b449995e40ff0d839
Tim Cooijmans <t.cooijmans@student.maastrichtuniversity.nl>
2013-11-16 07:22:33 +0100

    fix evaluation, implement alpha-beta pruning

--- ai.lisp
+++ ai.lisp
@@ -2,18 +2,19 @@
 
 (declaim (optimize (debug 3)))
 
-(defun minimax-decision (state)
-  (iter (for depth from 1 to 5)
-        (with value) (with best-move)
-        (multiple-value-setq (value best-move) (minimax state depth))
-        (print (list value best-move))
-        (finally (return best-move))))
+
+;; TODO: let evaluations be in [0,1] interpreted as probability of winning.
+;; then introduce a parameter *granularity* and transform the evaluations as
+;; (round (* *granularity* evaluation)). this gives control over the granularity
+;; of the evaluations; a coarser domain allows more pruning.
+
 
 ;; evaluation is from the perspective of player-to-move
 
 ;; an end-state is a state where no more moves can be made. the
 ;; player to move loses.
 (defun evaluate-end-state (state)
+  (declare (ignore state))
   -1)
 
 ;; just count advantage in number of pieces
@@ -21,12 +22,12 @@
   (iter (for i from 1 below (- *board-size* 1))
         (summing
          (iter (for j from 1 below (- *board-size* 1))
-               (summing (case (tile-owner (board-tile (state-board state) (v i j)))
-                          ((state-player state) 1)
-                          ((opponent (state-player state)) -1)
-                          (otherwise 0)))))))
+               (for owner = (tile-owner (board-tile (state-board state) (v i j))))
+               (summing (cond ((eq owner (state-player state)) 1)
+                              ((eq owner (opponent (state-player state))) -1)
+                              (t 0)))))))
 
-(defun minimax (state depth)
+(defun minimax (state depth alpha beta)
   (if (= depth 0)
       (evaluate-state state)
       (let ((moves (moves state)))
@@ -34,6 +35,19 @@
             (evaluate-end-state state)
             (iter (for move in moves)
                   (for breadcrumb = (apply-move state move))
-                  (finding move maximizing (minimax state (1- depth)) into (best-move value))
+                  (finding move
+                           maximizing (- (minimax state (1- depth) (- beta) (- alpha)))
+                           into (best-move value))
                   (unapply-move state breadcrumb)
-                  (finally (return (values (- value) best-move))))))))
+                  (setq alpha (max alpha value))
+                  (when (>= alpha beta)
+                    (finish))
+                  (finally (return (values value best-move))))))))
+
+(defun minimax-decision (state)
+  (iter (for depth from 0 to 5)
+        (with value) (with best-move)
+        (multiple-value-setq (value best-move) (minimax state depth most-negative-fixnum most-positive-fixnum))
+        (print (list value best-move))
+        (finally (return best-move))))
+
